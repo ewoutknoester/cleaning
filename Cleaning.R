0341b9c7de2c4cd537e475f62151422f2d6d2ec1
EvelienvdBerg <77284868+EvelienvdBerg@users.noreply.github.com>
2021-01-13 10:53:39 +0100

    Hoi even kijken of het nu wel werkt

--- Cleaning.R
+++ Cleaning.R
@@ -2,3 +2,14 @@
 //12 jan 
 10:25
 10:31
+mean(Production$SGR)
+mean(Growth$SGR)
+library(dplyr)
+Gemiddelde SGR van growth (>80% survival)
+aggregate(cbind(Growth$SGR) ~ Growth$size + Growth$species + Growth$date + Growth$cleaning, data = Growth, FUN='mean', na.rm = TRUE)
+Gemiddelde SGR van production (alle survival waardes waardoor je groei en survival in een hebt)
+aggregate(cbind(Production$SGR) ~ Production$size + Production$species + Production$date + Production$cleaning, data = Production, FUN='mean', na.rm = TRUE)
+df.production<- data.frame(aggregate(cbind(Production$SGR) ~ Production$size + Production$species + Production$date + Production$cleaning, data = Production, FUN='mean', na.rm = TRUE)
+print(df.production)
+write_xlsx(df.production,"C:/Users/eveli/Documents/Wageningen University/MSc Thesis/Nieuwe thesis - cleaning interval/Data\\Mean Production.xlsx")
+Growth %>% count(Growth$size, Growth$species, Growth$date, Growth$cleaning)
